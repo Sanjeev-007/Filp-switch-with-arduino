dc122503f5ecd66fc7f5e1957f5dc78eb72a3bab
Sanjeev-007 <53875709+Sanjeev-007@users.noreply.github.com>
2020-05-07 07:22:19 +0530

    Update index.php

--- ark-pro/index.php
+++ ark-pro/index.php
@@ -1,9 +1,19 @@
+<!--This is the updated version of code -->
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+  </head>
+  <body>
 <form 	action="sense.php"     method="get">
 <input type=hidden name=data value=1>
   <button> O N </button>
 </form>
-
+<br><br>
 <form 	action="sense.php"     method="get">
 <input type=hidden name=data value=0>
   <button> O F F </button>
 </form>
+  </body>
+</html>
