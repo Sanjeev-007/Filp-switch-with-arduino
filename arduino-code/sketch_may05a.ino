83e2cee00c1f6286f32d7dc113660d7463aa31f5
Sanjeev-007 <53875709+Sanjeev-007@users.noreply.github.com>
2020-05-07 07:37:35 +0530

    Add files via upload

---
+++ arduino-code/sketch_may05a.ino
@@ -0,0 +1,25 @@
+#include<Servo.h>
+Servo servo_switch;
+int pos=20;
+boolean vis_on=false,vis_off=false;
+void setup(){
+  Serial.begin(9600);
+  servo_switch.attach(8);
+}
+void loop(){
+  if(Serial.available()){
+    if(!vis_on || !vis_off){
+      int a=int(Serial.read());
+      if(a==1 && !vis_on){
+        servo_switch.write(0);
+        vis_on=true;
+      }else if(a!=1 && !vis_off){
+        servo_switch.write(40);
+        vis_off=true;
+      }
+    }else{
+      vis_on=false;
+      vis_off=false;
+    }
+  }
+}
